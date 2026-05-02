aaa2ea29a5c66031f3220a1f9539b16284820eee
Sneha Suman <snehasuman1119@gmail.com>
2026-05-02 22:21:11 +0530

    Time: 3 ms (91.63%), Space: 42.1 MB (70.36%) - LeetHub

---
+++ 0788-rotated-digits/0788-rotated-digits.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int rotatedDigits(int n) {
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (isGood(i)) count++;
+        }
+        return count;
+    }
+
+    private boolean isGood(int num) {
+        boolean changed = false;
+        while (num > 0) {
+            int d = num % 10;
+            if (d == 3 || d == 4 || d == 7) return false; // invalid
+            if (d == 2 || d == 5 || d == 6 || d == 9) changed = true;
+            num /= 10;
+        }
+        return changed;
+    }
+}
