a08ec2da8bd8574e2154d6f1d0d5f205d3a76402
Sneha Suman <snehasuman1119@gmail.com>
2026-05-03 23:38:58 +0530

    Time: 0 ms (100%), Space: 42.8 MB (42.97%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,10 +1,8 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        // Lengths must match, otherwise impossible
         if (s.length() != goal.length()) {
             return false;
         }
-        // Check if goal is a substring of s+s
         String doubled = s + s;
         return doubled.contains(goal);
     }
